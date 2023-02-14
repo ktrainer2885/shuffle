cce4de82326f9465a18d62581885f4add0969892
Ramirez, Rogelio S <rogelio.s.ramirez@nasa.gov>
2023-02-14 13:24:47 -0500

    Fixed shuffle. Eror from middleindex and begining array at 1 vs 0

--- deck.py
+++ deck.py
@@ -3,26 +3,27 @@ import card
 
 class Deck:
     deck = []
+    middleIndex = ""
 
     def __init__(self, size):
-        halfDeck = size // 2
+        halfDeck = (size // 2) + 1
+        self.middleIndex = halfDeck -1
 
-        for i in range(1, halfDeck + 1):
+        for i in range(1, halfDeck):
             practiceCard = card.Card(i, "L")
             self.deck.append(practiceCard)
 
-        for i in range(1, halfDeck):
+        for i in range(halfDeck, size + 1):
             practiceCard = card.Card(i, "R")
             self.deck.append(practiceCard)
 
     def shuffle(self):
-        middleIndex = len(self.deck)
-        left = self.deck[:middleIndex]
-        right = self.deck[middleIndex:]
+        left = self.deck[:self.middleIndex]
+        right = self.deck[self.middleIndex:]
 
         self.deck.clear()
 
-        for i in range(1, len(left)):
+        for i in range(len(left)):
             self.deck.append(left[i])
             if i < len(right):
                 self.deck.append(right[i])
